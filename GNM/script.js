5b16254b5ed3dd780967c33569af0a3333d9fad9
vincelabutay <vincelabutay@gmail.com>
2023-09-22 00:06:21 +0800

    modal window

--- GNM/script.js
+++ GNM/script.js
@@ -16,6 +16,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 let score = 20;
+let highScore = 0;
 
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 
@@ -33,6 +34,12 @@ document.querySelector('.check').addEventListener('click', function() {
 
 		document.querySelector('body').style.backgroundColor = '#60b347';
 		document.querySelector('.number').style.width = '30rem';
+
+		if(score > highScore) {
+			highScore = score;
+			document.querySelector('.highscore').textContent = highScore;
+		}
+
 	} else if ( guess > secretNumber) {
 		if (score > 0) {document.querySelector('.message').textContent = 'Too High';
 		score--;
